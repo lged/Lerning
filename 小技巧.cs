3d17f1fea5e7673125dfb4cc1085fec889cabd17
图坦卡蒙先生 <lged@163.com>
2020-01-11 08:45:44 +0800

    Update 小技巧.cs

--- 小技巧.cs
+++ 小技巧.cs
@@ -19,6 +19,13 @@ public static IEnumerable<int> getOdds(int n)
 #endregion
 
 
+#region virtual abstract
+重写：virtual 在基类可实现方法，派生类可 override 重写
+抽象：abstract 在基类不实现方法，派生类中必须用 override 实现，有 abstract 方法的类必须为 abstract class，且不能直接实例化
+覆盖：new 定义父类的同名方法，即非 virtual 方法
+#endregion
+
+
 #region 让接口实现方法
 interface IMy
 {

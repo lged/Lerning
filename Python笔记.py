3ac0b64d944f246e9245e8dfd6734fd5bc5077c5
图坦卡蒙先生 <lged@163.com>
2020-03-08 18:10:25 +0800

    Update Python笔记.py

--- Python笔记.py
+++ Python笔记.py
@@ -4,4 +4,11 @@ print '%s%s Hello World !!! %s' % (fg(1), bg(7), attr(0))
 print '%s This is Res %s' % (fg('red'), attr('reset'))
 print '{} This is Yellow bg {}'.format(fg('green'), attr('reset'))
 print '%s Hello World !!! %s' % (fg('#C0C0C0') + bg('#00005f'), attr('reset'))
-raw_input()
+
+# Python2 原生控制台颜色2
+from colored attr
+print attr('reset'),
+print '\033[1;36mCyan like Caribbean\033[1;m'
+print '\033[1;31mRed like Radish\033[1;m'
+print '\033[1;41mHighlighted Red like Radish\033[1;m'
+print '\033[1;42mHighlighted Green like Grass\033[1;m'

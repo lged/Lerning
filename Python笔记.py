c8439abf77d2ac4d2b28d38217dafc9cd4f7b08a
图坦卡蒙先生 <lged@163.com>
2020-03-08 17:46:08 +0800

    Update Python笔记.py

--- Python笔记.py
+++ Python笔记.py
@@ -3,4 +3,5 @@ from colored import fg, bg, attr
 print '%s%s Hello World !!! %s' % (fg(1), bg(7), attr(0))
 print '%s This is Res %s' % (fg('red'), attr('reset'))
 print '{} This is Yellow bg {}'.format(fg('green'), attr('reset'))
+print '%s Hello World !!! %s' % (fg('#C0C0C0') + bg('#00005f'), attr('reset'))
 raw_input()

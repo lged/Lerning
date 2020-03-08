a9a932e15d8186b4899a0553eb5cb5c1000fd324
图坦卡蒙先生 <lged@163.com>
2020-03-08 17:46:34 +0800

    Update Python笔记.py

--- Python笔记.py
+++ Python笔记.py
@@ -1,4 +1,4 @@
-#Python2 控制台文字颜色
+#Python2 控制台文字颜色 pip install colored
 from colored import fg, bg, attr
 print '%s%s Hello World !!! %s' % (fg(1), bg(7), attr(0))
 print '%s This is Res %s' % (fg('red'), attr('reset'))
